6affb84774d398dd194d4e54be59bdc782bd16c3
Takagisan <73755103+HasamiAini@users.noreply.github.com>
2021-01-23 11:36:33 -0800

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,6 +1,6 @@
 const config = {
-        botName: 'JINGAN BOT',
-        ownerName: 'Muhammad Iqbal',
-        youtube: 'https://m.youtube.com/channel/UCb6qlya4nhSYqNBdSQVes-A',
-        instagram: 'https://instagram.com/_miqbal.22',
+        botName: 'Bot_Chitanda Eru',
+        ownerName: 'Chitanda Eru',
+        youtube: 'https://www.youtube.com/channel/UCMICXHm2oq1h76wM_46O4Cg',
+        instagram: 'https://www.instagram.com/doujinmoee18/',
 }
