7fbe64a9e4a2a9e3c879e026a833013f197de889
Takagisan <73755103+HasamiAini@users.noreply.github.com>
2021-01-23 03:15:32 -0800

    Add files via upload

---
+++ database/menu/help.js
@@ -0,0 +1,144 @@
+const help = (prefix, ownerBot, botName) => {
+        return `
+「 *${botName}* 」
+
+👋️ *Yahalo Minna-San* , Jibun no namae
+  ╿
+  ┡➪☾ ⌨️BOT Commands⌨️ ☽
+  ┠➵ *${prefix}help*
+  ┠➵ ${prefix}timer*
+  ┠➵ ${prefix}bahasa*
+  ┠➵ ${prefix}kodenegara*
+  ┠➵ ${prefix}virtex*
+  ┠➵ ${prefix}owner*
+  ┠➵ ${prefix}infonomor*
+  ┠➵ ${prefix}spamcall*
+  ┠➵ ${prefix}testime*
+  ┠➵ ${prefix}blocklist*
+  ┠➵ ${prefix}chatlist*
+  ┠➵ ${prefix}apakah*
+  ╿
+➪☾ ⌨️Group Commands⌨️ ☽
+┠➵ ${prefix}demote*
+┠➵ ${prefix}promote*
+╿➵ ${prefix}hidetag*
+┠➵ ${prefix}listadmins*
+┠➵ ${prefix}tagall*
+┠➵ ${prefix}tagall2*
+┠➵ ${prefix}tagall3*
+┠➵ ${prefix}tagall4*
+┠➵ ${prefix}tagall5*
+┠➵ ${prefix}leave*
+┠➵ ${prefix}add*
+┠➵ ${prefix}kick*
+┠➵ ${prefix}linkgroup*
+┠➵ ${prefix}opengc*
+┠➵ ${prefix}closegc*
+┠➵ ${prefix}welcome*
+┠➵ ${prefix}del*
+┠➵ ${prefix}clone*
+➪☾ ⌨️Others Commands⌨️ ☽
+ ➥╿➵ ${prefix}wa.me*
+ ➥╿➵ ${prefix}quotes*
+ ➥╿➵ ${prefix}fml*
+ ➥╿➵ ${prefix}wiki*
+ ➥╿➵ ${prefix}nulis*
+ ➥╿➵ ${prefix}kbbi*
+ ➥╿➵ ${prefix}hilih*
+ ➥╿➵ ${prefix}quotemaker*
+ ➥╿➵ ${prefix}glitch*
+ ➥╿➵ ${prefix}chord*
+ ➥╿➵ ${prefix}lirik*
+ ➥╿➵ ${prefix}qrcode*
+ ➥╿➵ ${prefix}ocr*
+ ➥╿➵ ${prefix}bugreport*
+ ➥╿➵ ${prefix}kapankah*
+ ➥╿➵ ${prefix}bolehkah*
+ ➥╿➵ ${prefix}stickergif*
+ ➥╿➵ ${prefix}toimg*
+ ➥╿➵ ${prefix}infocuaca*
+ ➥╿➵ ${prefix}ninjalogo*
+ ➥╿➵ ${prefix}game*
+ ➥╿➵ ${prefix}fakta*
+ ➥╿➵ ${prefix}firetext*
+ ➥╿➵ ${prefix}cantikcek*
+ ➥╿➵ ${prefix}watak*
+ ➥╿➵ ${prefix}hobby*
+ ➥╿➵ ${prefix}logowolf*
+ ➥╿➵ ${prefix}bucin*
+ ➥╿➵ ${prefix}quotes2*
+ ➥╿➵ ${prefix}truth*
+ ➥╿➵ ${prefix}level*
+ ➥╿➵ ${prefix}fitnah*
+ ➥╿➵ ${prefix}ping*
+ ➥╿➵ ${prefix}jadwaltvnow*
+ ➥╿➵ ${prefix}afk*
+ ➥╿➵ ${prefix}shadow*
+ ➥╿➵ ${prefix}hobby*
+ ➥╿➵ ${prefix}logowolf*
+ ➥╿➵ ${prefix}bucin*
+ ➥╿➵ ${prefix}quotes2*
+ ➥╿➵ ${prefix}truth*
+ ➥╿➵ ${prefix}level*
+ ➥╿➵ ${prefix}fitnah*
+ ➥╿➵ ${prefix}ping*
+ ➥╿➵ ${prefix}jadwaltvnow*
+ ➥╿➵ ${prefix}randomkpop*
+ ➥╿➵ ${prefix}fml2*
+ ➥╿➵ ${prefix}ttp*
+ ➥╿➵ ${prefix}randomkpop*
+ ➥╿➵ ${prefix}fml2*
+ ➥╿➵ ${prefix}ttp*
+ ➪☾ ⌨️MEDIA Commands⌨️ ☽
+ ➥╿➵ ${prefix}tiktokstalk*
+ ➥╿➵ ${prefix}ytmp4*
+ ➥╿➵ ${prefix}trendtwit*
+ ➥╿➵ ${prefix}igstalk*
+ ➥╿➵ ${prefix}tomp3*
+ ➥╿➵ ${prefix}play*
+ ➥╿➵ ${prefix}tiktok*
+ ➪☾ ⌨️Anime Commands⌨️ ☽
+ ➥╿➵ ${prefix}loli*
+ ➥╿➵ ${prefix}animecry*
+ ➥╿➵ ${prefix}neonime*
+ ➥╿➵ ${prefix}pokemon*
+ ➥╿➵ ${prefix}nekonime*
+ ➥╿➵ ${prefix}neko*
+ ➥╿➵ ${prefix}animehug*
+ ➥╿➵ ${prefix}waifu*
+ ➥╿➵ ${prefix}randomanime*
+ ➥╿➵ ${prefix}husbu*
+ ➥╿➵ ${prefix}husbu2*
+ ➥╿➵ ${prefix}wait*
+ ➪☾ ⌨️Islam Commands⌨️ ☽
+ ➥╿➵ ${prefix}quran*
+ ➪☾ ⌨️Picture Commands⌨️ ☽
+ ➥╿➵ ${prefix}pinterest*
+ ➥╿➵ ${prefix}meme*
+ ➥╿➵ ${prefix}memeindo*
+ ➥╿➵ ${prefix}logowolf2*
+ ➥╿➵ ${prefix}phlogo*
+ ➥╿➵ ${prefix}neonlogo*
+ ➥╿➵ ${prefix}neonlogo2*
+ ➥╿➵ ${prefix}lionlogo*
+ ➥╿➵ ${prefix}jokerlogo*
+ ➥╿➵ ${prefix}shadow*
+ ➥╿➵ ${prefix}burnpaper*
+ ➥╿➵ ${prefix}coffee*
+ ➥╿➵ ${prefix}lovepaper*
+ ➥╿➵ ${prefix}woodblock*
+ ➥╿➵ ${prefix}qowheart*
+ ➥╿➵ ${prefix}mutgrass*
+ ➥╿➵ ${prefix}undergocean*
+ ➥╿➵ ${prefix}woodenboards*
+ ➥╿➵ ${prefix}wolfmetal*
+ ➥╿➵ ${prefix}metalictglow*
+ ➥╿➵ ${prefix}8bit*
+ ➥╿➵ ${prefix}pubglogo*
+ ➥╿➵ ${prefix}herrypotter*
+ ➪☾ ⌨️content 18+ Commands⌨️ ☽
+ ➥╿➵ ${prefix}doujinmoee*
+ ➥╿➵ ${prefix}randomhentai*
+ *GROUP BOT_TAKAGISAN:https://chat.whatsapp.com/EcIp7XK3fB70JzzTCNnfYz`
+}
+exports.help = help
---
+++ src/help.js
@@ -0,0 +1,144 @@
+const help = (prefix) => {
+	return `
+「 *BOT_Levi Vers.4.3* 」
+╿
+┡➪☾ ⌨️BOT Commands⌨️ ☽
+  ┠➵ *!help*
+  ┠➵ *!timer*
+  ┠➵ *!bahasa*
+  ┠➵ *!kodenegara*
+  ┠➵ *!virtex*
+  ┠➵ *!owner*
+  ┠➵ *!infonomor*
+  ┠➵ *!spamcall*
+  ┠➵ *!testime*
+  ┠➵ *!blocklist*
+  ┠➵ *!chatlist*
+  ┠➵ *!apakah*
+  ╿
+➪☾ ⌨️Group Commands⌨️ ☽
+┠➵ *!demote*
+┠➵ *!promote*
+╿➵ *!hidetag*
+┠➵ *!listadmins*
+┠➵ *!tagall*
+┠➵ *!tagall2*
+┠➵ *!tagall3*
+┠➵ *!tagall4*
+┠➵ *!tagall5*
+┠➵ *!leave*
+┠➵ *!add*
+┠➵ *!kick*
+┠➵ *!linkgroup*
+┠➵ *!opengc*
+┠➵ *!closegc*
+┠➵ *!welcome*
+┠➵ *!del*
+┠➵ *!simih*
+┠➵ *!clone*
+➪☾ ⌨️Others Commands⌨️ ☽
+ ➥╿➵ *!wa.me*
+ ➥╿➵ *!quotes*
+ ➥╿➵ *!fml*
+ ➥╿➵ *!wiki*
+ ➥╿➵ *!nulis*
+ ➥╿➵ *!kbbi*
+ ➥╿➵ *!hilih*
+ ➥╿➵ *!quotemaker*
+ ➥╿➵ *!glitch*
+ ➥╿➵ *!chord*
+ ➥╿➵ *!lirik*
+ ➥╿➵ *!qrcode*
+ ➥╿➵ *!ocr*
+ ➥╿➵ *!bugreport*
+ ➥╿➵ *!kapankah*
+ ➥╿➵ *!bolehkah*
+ ➥╿➵ *!stickergif*
+ ➥╿➵ *!toimg*
+ ➥╿➵ *!infocuaca*
+ ➥╿➵ *!ninjalogo*
+ ➥╿➵ *!game*
+ ➥╿➵ *!fakta*
+ ➥╿➵ *!firetext*
+ ➥╿➵ *!cantikcek*
+ ➥╿➵ *!watak*
+ ➥╿➵ *!hobby*
+ ➥╿➵ *!logowolf*
+ ➥╿➵ *!bucin*
+ ➥╿➵ *!quotes2*
+ ➥╿➵ *!truth*
+ ➥╿➵ *!level*
+ ➥╿➵ *!fitnah*
+ ➥╿➵ *!ping*
+ ➥╿➵ *!jadwaltvnow*
+ ➥╿➵ *!afk*
+ ➥╿➵ *!shadow*
+ ➥╿➵ *!hobby*
+ ➥╿➵ *!logowolf*
+ ➥╿➵ *!bucin*
+ ➥╿➵ *!quotes2*
+ ➥╿➵ *!truth*
+ ➥╿➵ *!level*
+ ➥╿➵ *!fitnah*
+ ➥╿➵ *!ping*
+ ➥╿➵ *!jadwaltvnow*
+ ➥╿➵ *!randomkpop*
+ ➥╿➵ *!fml2*
+ ➥╿➵ *!ttp*
+ ➥╿➵ *!randomkpop*
+ ➥╿➵ *!fml2*
+ ➥╿➵ *!ttp*
+ ➪☾ ⌨️MEDIA Commands⌨️ ☽
+ ➥╿➵ *!tiktokstalk*
+ ➥╿➵ *!ytmp4*
+ ➥╿➵ *!trendtwit*
+ ➥╿➵ *!igstalk*
+ ➥╿➵ *!tomp3*
+ ➥╿➵ *!play*
+ ➥╿➵ *!tiktok*
+ ➪☾ ⌨️Anime Commands⌨️ ☽
+ ➥╿➵ *!loli*
+ ➥╿➵ *!animecry*
+ ➥╿➵ *!neonime*
+ ➥╿➵ *!pokemon*
+ ➥╿➵ *!nekonime*
+ ➥╿➵ *!neko*
+ ➥╿➵ *!animehug*
+ ➥╿➵ *!waifu*
+ ➥╿➵ *!randomanime*
+ ➥╿➵ *!husbu*
+ ➥╿➵ *!husbu2*
+ ➥╿➵ *!wait*
+ ➪☾ ⌨️Islam Commands⌨️ ☽
+ ➥╿➵ *!quran*
+ ➪☾ ⌨️Picture Commands⌨️ ☽
+ ➥╿➵ *!pinterest*
+ ➥╿➵ *!meme*
+ ➥╿➵ *!memeindo*
+ ➥╿➵ *!logowolf2*
+ ➥╿➵ *!phlogo*
+ ➥╿➵ *!neonlogo*
+ ➥╿➵ *!neonlogo2*
+ ➥╿➵ *!lionlogo*
+ ➥╿➵ *!jokerlogo*
+ ➥╿➵ *!shadow*
+ ➥╿➵ *!burnpaper*
+ ➥╿➵ *!coffee*
+ ➥╿➵ *!lovepaper*
+ ➥╿➵ *!woodblock*
+ ➥╿➵ *!qowheart*
+ ➥╿➵ *!mutgrass*
+ ➥╿➵ *!undergocean*
+ ➥╿➵ *!woodenboards*
+ ➥╿➵ *!wolfmetal*
+ ➥╿➵ *!metalictglow*
+ ➥╿➵ *!8bit*
+ ➥╿➵ *!pubglogo*
+ ➥╿➵ *!herrypotter*
+ ➪☾ ⌨️content 18+ Commands⌨️ ☽
+ ➥╿➵ *!doujinmoee*
+ ➥╿➵ *!randomhentai*
+`
+}
+
+exports.help = help
